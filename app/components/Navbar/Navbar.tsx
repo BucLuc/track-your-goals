ff10e69e1307ec41fbfdbef3d314361e864c26dd
BucLuc <buc.kneuluc@gmail.com>
2024-05-22 21:57:00 +0200

    responsive navbar

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -14,21 +14,27 @@ const Navbar: React.FC<NavbarProps> = ({ activeLink, user, photoURL }) => {
     const [isHovering, setIsHovering] = useState(false);
 
     const actualURL = photoURL ? photoURL : user && user.photoURL ? user.photoURL : "/img/Avatars/default-avatar.png";
+    const [menuOpen, setMenuOpen] = useState(false);
 
     return (
     <nav className={styles.nav}>
-        <div className={styles["nav-content"]}>
+        <div className={`${styles["nav-content"]} ${menuOpen ? styles['nav-open'] : ''}`}>
             <div className={styles.logo}>
                 <a href='/'>
                 <img alt="logo" src="/img/logo-icon.png"/>
                 <img alt="logo-text" src='/img/logo-text.png' />
                 </a>
             </div>
-            <ul>
+            <ul className={`${styles.links} ${menuOpen ? styles.open : ''}`}>
                 <li className={activeLink === "DASHBOARD" ? styles.active : ""}><a href='/dashboard'>Dashboard</a></li>
                 <li className={activeLink === "ACTIVITIES" ? styles.active : ""}><a href='/activities'>Aktivitäten</a></li>
                 <li className={activeLink === "TEMPLATE" ? styles.active : ""}><a href='/template'>Wochenvorlage</a></li>
             </ul>
+            <div className={styles.burger} onClick={() => setMenuOpen(!menuOpen)}>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                    <div className={styles.bar}></div>
+                </div>
             <div className={styles.user}
                 onMouseEnter={() => setIsHovering(true)}
                 onMouseLeave={() => setIsHovering(false)}>
